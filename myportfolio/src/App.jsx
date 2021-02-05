d197a98a7254854bad34cf1cd952eafd730ca2b8
Tarun1108 <tarunrajpoot1108@gmail.com>
2021-02-05 12:53:33 +0530

    inital commit completed mynavbar component

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import  React  from  'react';
+import './App.css';
+import MyNavbar from "./components/my-navbar/my-navbar.component";
+
+const App=()=> {
+  return (
+      <div >
+        <MyNavbar />
+        My Portfolio
+        </div>
+    );
+};
+
+export default App;
