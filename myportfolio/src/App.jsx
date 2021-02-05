fb63ad410a4a53d184a1bc5608f06293063efe52
Tarun1108 <tarunrajpoot1108@gmail.com>
2021-02-05 23:24:38 +0530

    Skills page added

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -5,9 +5,11 @@ import MyCarousal from "./components/my-carousal/my-carousal.component";
 import  TitleMessage from  "./components/title-message/title-message-component";
 import About from './pages/about/about.component'
 import Container from "react-bootstrap/Container";
+import Skills from './pages/skills/skills.component';
 
 import { Parallax } from "react-parallax";
 import Fade from "react-reveal/Fade";
+import { skills } from './pages/skills/skills-data';
 
 const App=()=> {
   return (
@@ -22,12 +24,20 @@ const App=()=> {
                         <Fade duration={500}>
                                 <About />
                         </Fade>    
-                    </Container>
-                      
+                    </Container>         
             </Parallax>
-            
+          </div>
+       {/*Skills*/}
+       <div>
+               <Container className="container-box rounded">
+                        <Fade duration={500}>
+                                    <Skills />
+                         </Fade>    
+                </Container>         
         </div>
-       
+
+
+        
   
         </div>
     );
